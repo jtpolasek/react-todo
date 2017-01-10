8ae7dfb84d4f23467893b55c6950582f890aa305
Todd <tpolasek@gmail.com>
2017-01-10 12:42:44 -0800

    updated todo functionality

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -11,4 +11,15 @@ describe('TodoApp', () => {
   it('should exist', () => {
     expect(TodoApp).toExist();
   });
+
+  it('should add todo state on handleAddToDo', () =>{
+    var todoText = "new todo test";
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddToDo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+
+  });
 });
