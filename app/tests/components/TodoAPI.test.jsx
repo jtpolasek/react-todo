aa66baa8351c7aa6da4cf352c430b4e6e53c8904
Todd <tpolasek@gmail.com>
2017-01-18 16:19:09 -0800

    add filter function

--- app/tests/components/TodoAPI.test.jsx
+++ app/tests/components/TodoAPI.test.jsx
@@ -49,4 +49,47 @@ describe('TodoAPI', () => {
       expect(todos).toEqual(actualTodos);
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: "some Text",
+      completed: true
+    }, {
+      id: 2,
+      text: "Other some here",
+      completed: false
+    }, {
+      id: 3,
+      text: "Other text here",
+       completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () =>{
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toEqual(3);
+    });
+
+    it('should return only non completed items if showCompleted is false', () =>{
+        var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+        expect(filteredTodos.length).toEqual(1);
+    });
+
+    it('should sort by completed status', () =>{
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', () =>{
+        var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+        expect(filteredTodos.length).toEqual(2);
+    });
+
+    it('should return all todos if searchText is empty', () =>{
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toEqual(3);
+    });
+
+
+  })
 });
