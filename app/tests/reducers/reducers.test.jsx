15ad57ec2b0dd03cc456fc394990586a883e9449
Todd <tpolasek@gmail.com>
2017-02-22 16:35:49 -0800

    add reducer and test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -12,7 +12,7 @@ describe('Reducers', () =>{
       var res = reducers.searchTextReducer(df(''), df(action));
       expect(res).toEqual(action.searchText);
     });
-
+  });
 
     describe('showCompletedReducer', () =>{
       it('should toggle completed', () =>{
@@ -23,5 +23,39 @@ describe('Reducers', () =>{
         expect(res).toEqual(true);
       });
     });
-  });
+
+    describe('todosReducer', () =>{
+      it('should add new todo', () =>{
+        var action = {
+          type: 'ADD_TODO',
+          text: 'Walk the dog'
+        };
+        var res = reducers.todosReducer(df([]),df(action));
+        expect(res.length).toEqual(1);
+        expect(res[0].text).toEqual(action.text);
+      });
+    });
+
+      describe('Toggle completed todo', () =>{
+        it('should toggle the  todo', () =>{
+          var todo = [
+            {
+              id: 1,
+              text: 'test todo',
+              completed: false,
+              createdAt: 123,
+              completedAt: undefined
+            }
+          ]
+          var action = {
+            type: 'TOGGLE_TODO',
+            id: 1
+          };
+          var res = reducers.todosReducer(df(todo),df(action));
+          expect(res[0].id).toEqual(1);
+          expect(res[0].completed).toEqual(true);
+          expect(res[0].createdAt).toEqual(123);
+          expect(res[0].completedAt).toNotEqual(undefined);
+        });
+      });
 });
