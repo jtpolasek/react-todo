d268fdb2e83ae834325f7db18a38c03b768f4a7c
Todd <tpolasek@gmail.com>
2017-02-28 12:23:47 -0800

    refactor todoSearch to work with redux store

--- app/app.jsx
+++ app/app.jsx
@@ -11,10 +11,7 @@ store.subscribe(() => {
   console.log('New state', store.getState());
 });
 
-store.dispatch(actions.addTodo('Clean the yard'));
-store.dispatch(actions.setSearchText('yard'));
-store.dispatch(actions.toggleShowCompleted());
-//var Main = require('Main');
+
 
 //Load foundation
 $(document).foundation();
--- app/tests/components/TodoSearch.test.jsx
+++ app/tests/components/TodoSearch.test.jsx
@@ -5,32 +5,37 @@ var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 
-var TodoSearch = require('TodoSearch');
+import {TodoSearch} from 'TodoSearch';
 
 describe('TodoSearch', () => {
   it('should exist', () => {
     expect(TodoSearch).toExist();
   });
 
-  it('should call onSearch with entered input text', () =>{
+  it('should dispatch SET_SEARCH_TEXT on input change', () =>{
     var spy = expect.createSpy();
-    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>)
+    var searchText = 'Dog';
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText
+    };
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch dispatch={spy}/>)
     var $el = $(ReactDOM.findDOMNode(todoSearch));
-    var someText = 'search for this';
-    todoSearch.refs.searchText.value = someText;
+    todoSearch.refs.searchText.value = searchText;
     TestUtils.Simulate.change(todoSearch.refs.searchText);
 
-    expect(spy).toHaveBeenCalledWith(false, someText);
+    expect(spy).toHaveBeenCalledWith(action);
     });
 
-    it('should call onSearch with proper checked value', () =>{
+    it('should dispatch TOGGLE_SHOW_COMPLETED when checkbox checked', () =>{
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      }
       var spy = expect.createSpy();
-      var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>)
+      var todoSearch = TestUtils.renderIntoDocument(<TodoSearch dispatch={spy}/>)
 
       todoSearch.refs.showCompleted.checked = true;
       TestUtils.Simulate.change(todoSearch.refs.showCompleted);
-      expect(spy).toHaveBeenCalledWith(true, '');
+      expect(spy).toHaveBeenCalledWith(action);
       });
-
-
 });
